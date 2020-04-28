d2411f6890acd918914715b82ab3aaa2b3f18020
dansobolev <daniil.sob56@gmail.com>
2020-04-28 11:58:46 +0300

    fixed

--- matrixMulti.c
+++ matrixMulti.c
@@ -6,86 +6,52 @@
 #define N 3
 
 
-static PyObject* py_echo( PyObject* self, PyObject* args ) {
+static PyObject* MatrixMultiplication( PyObject* self, PyObject* args ) {
 
+    PyObject* list1;
+    PyObject* list2;
     int i, j, k;
-    int **A = (int**)malloc(N * sizeof(int*));
-    int **B = (int**)malloc(N * sizeof(int*));
-    int **C = (int**)malloc(N * sizeof(int*));
-    for (i = 0; i < N; i++)
-    {
-        A[i] = (int*)malloc(N * sizeof(int));
-        B[i] = (int*)malloc(N * sizeof(int));
-        C[i] = (int*)malloc(N * sizeof(int));
+    int len1; len2;
+
+    if (!PyArg_ParseTuple(args, "Ob", &list1, &list2)){
+        return NULL;
     }
+    len1 = PySequence_Fast_GET_SIZE(list1);
+    len2 = PySequence_Fast_GET_SIZE(PySequence_Fast_GET_ITEM(list,2));
 
-
-
-    srand(time(NULL));
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-        {
-            A[i][j] = rand() % 10;
-            B[i][j] = rand() % 10;
+    for (c=0; c<len1; c++){
+        PyObject* item1 = PySequence_Fast_GET_ITEM(list1, c);
+        for (i=0; i<len2; i++){
+            PyObject* item2 = PySequence_Fast_GET_ITEM(item1, i);
+            for (j=0; j<len2; j++){
+                PyObject* item3 = PySequence_Fast_GET_ITEM(item1, j);
+                C[i][j] = 0;
+                for (k=0; k<len2; k++){
+                    C[i][j] += item2*item3
+                }
+            }
         }
-
-
-
-    for(i = 0; i < N; i++)
-        for(j = 0; j < N; j++)
-        {
-            C[i][j] = 0;
-            for(k = 0; k < N; k++)
-            C[i][j] += A[i][k] * B[k][j];
-        }
-
-
-
-
-    printf("matrix A\n");
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++)
-            printf("%d ", A[i][j]);
-        printf("\n");
     }
-    printf("\nmatrix B\n");
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++)
-            printf("%d ", B[i][j]);
-        printf("\n");
-    }
-    printf("\nthe result of multiplying\n");
-    for (i = 0; i < N; i++)
+
+    for (i = 0; i < len1; i++)
     {
         for (j = 0; j < N; j++)
             printf("%3d ", C[i][j]);
         printf("\n");
     }
-
-
-
-
-
-
-
-    for (i = 0; i < N; i++)
-    {
-        free(A[i]);
-        free(B[i]);
-        free(C[i]);
-    }
-    free(A);
-    free(B);
-    free(C);
+    `
 
 
     return Py_None;
 }
 
 static PyMethodDef matrixMulti_methods[] = {
-   { "matrixMultiplication", py_echo, METH_NOARGS, "echo function" },
+    wd{
+        "matrixMultiplication",
+        MatrixMultiplication,
+        METH_NOARGS,
+        "multiplication between two matrices"
+    },
    { NULL, NULL }
 };
 
---
+++ program_code.c
@@ -0,0 +1,71 @@
+int i, j, k;
+    int **A = (int**)malloc(N * sizeof(int*));
+    int **B = (int**)malloc(N * sizeof(int*));
+    int **C = (int**)malloc(N * sizeof(int*));
+    for (i = 0; i < N; i++)
+    {
+        A[i] = (int*)malloc(N * sizeof(int));
+        B[i] = (int*)malloc(N * sizeof(int));
+        C[i] = (int*)malloc(N * sizeof(int));
+    }
+
+
+
+    srand(time(NULL));
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+        {
+            A[i][j] = rand() % 10;
+            B[i][j] = rand() % 10;
+        }
+
+
+
+    for(i = 0; i < N; i++)
+        for(j = 0; j < N; j++)
+        {
+            C[i][j] = 0;
+            for(k = 0; k < N; k++)
+            C[i][j] += A[i][k] * B[k][j];
+        }
+
+
+
+
+    printf("matrix A\n");
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+            printf("%d ", A[i][j]);
+        printf("\n");
+    }
+    printf("\nmatrix B\n");
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+            printf("%d ", B[i][j]);
+        printf("\n");
+    }
+    printf("\nthe result of multiplying\n");
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+            printf("%3d ", C[i][j]);
+        printf("\n");
+    }
+
+
+
+
+
+
+
+    for (i = 0; i < N; i++)
+    {
+        free(A[i]);
+        free(B[i]);
+        free(C[i]);
+    }
+    free(A);
+    free(B);
+    free(C);
