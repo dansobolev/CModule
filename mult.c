d880ba863e52daeaa92ade7c21fe09daf93d2859
dansobolev <daniil.sob56@gmail.com>
2020-05-03 16:39:00 +0300

    final version

--- mult.c
+++ mult.c
@@ -11,9 +11,9 @@ static PyObject *matrix_mul(PyObject *self, PyObject *args) {
     int **B = (int**)malloc(N * sizeof(int*));
     int **C = (int**)malloc(N * sizeof(int*));
     for (i=0; i<N; i++){
-	A[i] = (int*)malloc(N * sizeof(int));
-	B[i] = (int*)malloc(N * sizeof(int));
-	C[i] = (int*)malloc(N * sizeof(int));
+        A[i] = (int*)malloc(N * sizeof(int));
+        B[i] = (int*)malloc(N * sizeof(int));
+        C[i] = (int*)malloc(N * sizeof(int));
     }
 
     if (!PyArg_ParseTuple(args, "OO", &list1, &list2)){
@@ -22,42 +22,44 @@ static PyObject *matrix_mul(PyObject *self, PyObject *args) {
 
     for (i=0; i<N; i++){
         PyObject* item1 = PySequence_Fast_GET_ITEM(list1, i);
-	for (j=0; j<N; j++){
-	    res1 = PyFloat_AsDouble(PySequence_Fast_GET_ITEM(item1, j));
-	    A[i][j] = res1;
-	}
+        for (j=0; j<N; j++){
+            res1 = PyFloat_AsDouble(PySequence_Fast_GET_ITEM(item1, j));
+            A[i][j] = res1;
+        }
     }
 
 
     for (i=0; i<N; i++){
         PyObject* item2 = PySequence_Fast_GET_ITEM(list2, i);
-	for (j=0; j<N; j++){
-	    res2 = PyFloat_AsDouble(PySequence_Fast_GET_ITEM(item2, j));
-	    B[i][j] = res2;
-	}
+        for (j=0; j<N; j++){
+            res2 = PyFloat_AsDouble(PySequence_Fast_GET_ITEM(item2, j));
+            B[i][j] = res2;
+        }
     }
 
 
     for(i=0; i<N; i++){
-	for(j=0; j<N; j++){
-	    C[i][j] = 0;
-	    for(k=0; k<N; k++){
-		C[i][j] += A[i][k] * B[k][j];
-	    }
-	}
+        for(j=0; j<N; j++){
+            C[i][j] = 0;
+            for(k=0; k<N; k++){
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
     }
 
 
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++)
-            printf("%3d ", C[i][j]);
-        printf("\n");
+    PyObject *my_list = PyList_New(0);
+    for (i=0; i<N; i++){
+        PyObject *new_list = PyList_New(3);
+        for (int j=0; j<N; j++){
+            PyObject *the_object = PyLong_FromSsize_t(C[i][j]);
+            PyList_SetItem(new_list, j, the_object);
+        }
+
+        PyList_Append(my_list, new_list);
     }
 
-
-
-    return Py_None;
+    return my_list;
 }
 
 static PyMethodDef ownmod_methods[] = {
